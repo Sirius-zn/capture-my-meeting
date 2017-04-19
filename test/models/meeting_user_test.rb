c531dc51f4c9c20aa0551fa29a9e102709a448b0
Mark Diez <markediez@gmail.com>
2017-04-19 16:05:16 -0700

    Add test for meeting_user

--- test/models/meeting_user_test.rb
+++ test/models/meeting_user_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class MeetingUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def setup
+		@mu = MeetingUser.new(user_id: 31, meeting_id: 3)		
+	end
+	
+	test "should be valid" do
+		assert @mu.valid?
+	end
+
+
+	test "user_id should be present" do
+		@mu.user_id = nil
+		assert_not @mu.valid?
+	end
+	
+	test "meeting_id should be present" do
+		@mu.meeting_id = nil
+		assert_not @mu.valid?
+	end
+
 end
