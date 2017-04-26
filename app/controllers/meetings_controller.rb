7b14fc63ec4c07f3f0d1a52667b56926d00af977
Mark Diez <markediez@gmail.com>
2017-04-26 16:00:45 -0700

    Create upload folder when on meeting create

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,68 +1,83 @@
 class MeetingsController < ApplicationController
-	skip_before_filter :verify_authenticity_token
-	before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+  skip_before_filter :verify_authenticity_token
+  before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+  after_commit :create_upload_folder
 
-    include MeetingsHelper
+  include MeetingsHelper
 
-    def join
-        unless params[:code].present? || params[:password].present?
-            redirect_to meeting_url
-        end
-
-        @meeting = Meeting.find_by(:code => params[:code])
-        if @meeting.authenticate?(params[:password])
-            mu = MeetingUser.find_by(:user_id => current_user.id, :meeting_id => @meeting.id)
-            if (mu.nil?)
-                mu = MeetingUser.new(:user_id => current_user.id, :meeting_id => @meeting.id, :user_role => params[:user_role])
-                mu.save
-            end
-
-            redirect_to @meeting
-        end
+  def join
+    unless params[:code].present? || params[:password].present?
+      redirect_to meeting_url
     end
 
-    def show
-        @mu = MeetingUser.find_by(:meeting_id => @meeting.id, :user_id => current_user.id)
+    @meeting = Meeting.find_by(:code => params[:code])
+    if @meeting.authenticate?(params[:password])
+      mu = MeetingUser.find_by(:user_id => current_user.id, :meeting_id => @meeting.id)
+      if (mu.nil?)
+        mu = MeetingUser.new(:user_id => current_user.id, :meeting_id => @meeting.id, :user_role => params[:user_role])
+        mu.save
+      end
 
-        # TODO: Output error
-        redirect_to meetings_url if @mu.nil?
+      redirect_to @meeting
+    end
+  end
+
+  def show
+    @mu = MeetingUser.find_by(:meeting_id => @meeting.id, :user_id => current_user.id)
+
+    # TODO: Output error
+    redirect_to meetings_url if @mu.nil?
+  end
+
+  # GET /meetings/new
+  def new
+  end
+
+  # POST /meetings
+  def create
+    @meeting = Meeting.new :code => generate_code, :password => generate_password, :user_id => current_user.id
+
+    respond_to do |format|
+      if @meeting.save
+        @mu = MeetingUser.new(:meeting_id => @meeting.id, :user_id => current_user.id, :user_role => params[:user_role])
+        @mu.save!
+        format.html { redirect_to @meeting, notice: 'Meeting created.' }
+        format.json { render action: 'show', status: :created, location: @meeting }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @meeting.errors, status: :unporcessable_entity }
+      end
+    end
+  end
+
+  # GET /meetings/1/edit
+  def edit
+  end
+
+  # PATCH /meetings/1
+  def update
+  end
+
+  private
+    def set_meeting
+      @meeting = Meeting.find(params[:id])
     end
 
-    # GET /meetings/new
-    def new
+    def meeting_params
+      params.require(:user_role)
     end
 
-    # POST /meetings
-    def create
-        @meeting = Meeting.new :code => generate_code, :password => generate_password, :user_id => current_user.id
+    def create_upload_folder
+      # Create /uploads folder if it doesn't exist
+      dirname = "#{Rails.root}/uploads"
+      unless File.directory?(dirname)
+        system("mkdir #{dirname}")
+      end
 
-        respond_to do |format|
-            if @meeting.save
-                @mu = MeetingUser.new(:meeting_id => @meeting.id, :user_id => current_user.id, :user_role => params[:user_role])
-                @mu.save!
-                format.html { redirect_to @meeting, notice: 'Meeting created.' }
-                format.json { render action: 'show', status: :created, location: @meeting }
-            else
-                format.html { render action: 'new' }
-                format.json { render json: @meeting.errors, status: :unporcessable_entity }
-            end
-        end
+      # Create folder to store images if it doesn't exist
+      dirname = "#{dirname}/#{@meeting.id}"
+      unless File.directory?(dirname)
+        system("mkdir #{dirname}")
+      end
     end
-
-    # GET /meetings/1/edit
-    def edit
-    end
-
-    # PATCH /meetings/1
-    def update
-    end
-
-    private
-        def set_meeting
-            @meeting = Meeting.find(params[:id])
-        end
-
-        def meeting_params
-            params.require(:user_role)
-        end
 end
