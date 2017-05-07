dec2359c25916659778a80e7edcbae56213a286b
Mark Diez <markediez@gmail.com>
2017-05-07 14:54:45 -0700

    Add notices and remove commented code

--- app/controllers/meetings_controller.rb
+++ app/controllers/meetings_controller.rb
@@ -1,68 +1,76 @@
 class MeetingsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  before_action :set_meeting, only: [:show, :edit, :update, :destroy]
+    skip_before_filter :verify_authenticity_token
+    before_action :set_meeting, only: [:show, :edit, :update, :destroy]
 
-  include MeetingsHelper
+    include MeetingsHelper
 
-  def join
-    unless params[:code].present? || params[:password].present?
-      redirect_to meeting_url
+    def index
+        flash[:notice] = params[:notice] 
     end
 
-    @meeting = Meeting.find_by(:code => params[:code])
-    if @meeting.authenticate?(params[:password])
-      mu = MeetingUser.find_by(:user_id => current_user.id, :meeting_id => @meeting.id)
-      if (mu.nil?)
-        mu = MeetingUser.new(:user_id => current_user.id, :meeting_id => @meeting.id, :user_role => params[:user_role])
-        mu.save
-      end
+    def join
+        unless params[:code].present? || params[:password].present?
+            redirect_to meeting_url
+        end
 
-      redirect_to @meeting
+        @meeting = Meeting.find_by(:code => params[:code])
+        respond_to do |format|
+            if @meeting && @meeting.authenticate?(params[:password])
+                mu = MeetingUser.find_by(:user_id => current_user.id, :meeting_id => @meeting.id)
+                if (mu.nil?)
+                    mu = MeetingUser.new(:user_id => current_user.id, :meeting_id => @meeting.id, :user_role => params[:user_role])
+                    mu.save
+                end
+
+                format.html { redirect_to @meeting, notice: "Meeting joined." }
+            else
+                format.html { redirect_to action: :index, notice: "Meeting not found." }
+            end
+        end
     end
-  end
 
-  def show
-    @mu = MeetingUser.find_by(:meeting_id => @meeting.id, :user_id => current_user.id)
+    def show
+        @mu = MeetingUser.find_by(:meeting_id => @meeting.id, :user_id => current_user.id)
 
-    # TODO: Output error
-    redirect_to meetings_url if @mu.nil?
-  end
-
-  # GET /meetings/new
-  def new
-  end
-
-  # POST /meetings
-  def create
-    @meeting = Meeting.new :code => generate_code, :password => generate_password, :user_id => current_user.id
-
-    respond_to do |format|
-      if @meeting.save
-        @mu = MeetingUser.new(:meeting_id => @meeting.id, :user_id => current_user.id, :user_role => params[:user_role])
-        @mu.save!
-        format.html { redirect_to @meeting, notice: 'Meeting created.' }
-        format.json { render action: 'show', status: :created, location: @meeting }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @meeting.errors, status: :unporcessable_entity }
-      end
+        # TODO: Output error
+        redirect_to meetings_url if @mu.nil?
     end
-  end
 
-  # GET /meetings/1/edit
-  def edit
-  end
+    # GET /meetings/new
+    def new
+    end
 
-  # PATCH /meetings/1
-  def update
-  end
+    # POST /meetings
+    def create
+        @meeting = Meeting.new :code => generate_code, :password => generate_password, :user_id => current_user.id
 
-  private
+        respond_to do |format|
+            if @meeting.save
+                @mu = MeetingUser.new(:meeting_id => @meeting.id, :user_id => current_user.id, :user_role => params[:user_role])
+                @mu.save!
+                format.html { redirect_to @meeting, notice: 'Meeting created.' }
+                format.json { render action: 'show', status: :created, location: @meeting }
+            else
+                format.html { render action: 'new' }
+                format.json { render json: @meeting.errors, status: :unprocessable_entity }
+            end
+        end
+    end
+
+    # GET /meetings/1/edit
+    def edit
+    end
+
+    # PATCH /meetings/1
+    def update
+    end
+
+    private
     def set_meeting
-      @meeting = Meeting.find(params[:id])
+        @meeting = Meeting.find(params[:id])
     end
 
     def meeting_params
-      params.require(:user_role)
+        params.require(:user_role)
     end
 end
