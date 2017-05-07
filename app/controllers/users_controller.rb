af37f1a55dcca03b2a8bafb1bd562d21af6a52c0
Mark Diez <markediez@gmail.com>
2017-05-07 10:37:31 -0700

    Fix inheritence

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-class UsersController < ActiveRecord::Base
+class UsersController < ApplicationController
 	skip_before_filter :verify_authenticity_token
 	before_action :set_user, only: [:show, :edit, :update, :destroy]
 	def index
