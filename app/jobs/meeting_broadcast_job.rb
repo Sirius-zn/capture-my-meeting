320ddd51a87f8f52b8476f034bf97bc89525f81b
Mark Diez <markediez@gmail.com>
2017-05-07 18:50:51 -0700

    Don't use end as a var name

--- app/jobs/meeting_broadcast_job.rb
+++ app/jobs/meeting_broadcast_job.rb
@@ -1,19 +1,19 @@
 class MeetingBroadcastJob < ApplicationJob
-  queue_as :default
+    queue_as :default
 
-  # perform job
-  # filename - image filename
-  # image - the image itself in base64
-  # src - {x,y} of bounding box start
-  # end - {x,y} of bounding box end
-  def perform(filename, image, id, src, end)
-    dirname = "#{Rails.root}/uploads/#{id}"
+    # perform job
+    # filename - image filename
+    # image - the image itself in base64
+    # src - {x,y} of bounding box start
+    # end - {x,y} of bounding box end
+    def perform(filename, image, id, src, dest)
+        dirname = "#{Rails.root}/uploads/#{id}"
 
-    # Write Image to Disk
-    png = Base64.decode64(image['data:image/png;base64,'.length .. -1])
-    File.open("#{dirname}/#{filename}", 'wb') { |f| f.write(png) }
-    status = true;
+        # Write Image to Disk
+        png = Base64.decode64(image['data:image/png;base64,'.length .. -1])
+        File.open("#{dirname}/#{filename}", 'wb') { |f| f.write(png) }
+        status = true;
 
-    # ActionCable.server.broadcast "meetings_#{id}", image: image, status: status
-  end
+        ActionCable.server.broadcast "meetings_#{id}", image: image, status: status
+    end
 end
