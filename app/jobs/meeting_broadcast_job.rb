7b7ad0d402199db92974ab47725de02549f723ad
Mark Diez <markediez@gmail.com>
2017-05-09 23:09:12 -0700

    Add commented code to return fixed_image

--- app/jobs/meeting_broadcast_job.rb
+++ app/jobs/meeting_broadcast_job.rb
@@ -1,5 +1,6 @@
 class MeetingBroadcastJob < ApplicationJob
     queue_as :default
+    @@img_cache = {}
 
     # perform job
     # filename - image filename
@@ -12,8 +13,30 @@ class MeetingBroadcastJob < ApplicationJob
         # Write Image to Disk
         png = Base64.decode64(image['data:image/png;base64,'.length .. -1])
         File.open("#{dirname}/#{filename}", 'wb') { |f| f.write(png) }
-        status = true;
 
+        status = true
         ActionCable.server.broadcast "meetings_#{id}", image: image, status: status
+
+        # Once your side is set up Sirius, uncomment the lines below and delete line 17-18
+        # Path to merged picture
+        # path = "#{Rails.root}/uploads/#{id}/test.png"
+        # fixed_image = nil
+        # status = false
+        # if File.exists?(path)
+        #     File.open(path, 'rb') do |file|
+        #         fixed_image = 'data:image/png;base64,'
+        #         if File.exists?(path)
+        #             img = Base64.encode64(file.read)
+        #             hex = Digest::MD5.hexdigest(img)
+        #             if @@img_cache[id] == nil || (hex != @@img_cache[id])
+        #                 @@img_cache[id] = hex
+        #                 fixed_image += img
+        #                 status = true
+        #             end
+        #         end
+        #     end
+        # end
+        # ActionCable.server.broadcast "meetings_#{id}", image: fixed_image, status: status
     end
+
 end
